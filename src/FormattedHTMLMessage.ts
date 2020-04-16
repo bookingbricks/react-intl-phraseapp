0b12d9f24dd8b251130b2a8bd1596ccfc8af615b
Jonatan Lundqvist Meden <2929793+jontelm@users.noreply.github.com>
2020-04-16 12:58:43 +0200

    fix

--- src/FormattedHTMLMessage.ts
+++ src/FormattedHTMLMessage.ts
@@ -1,22 +1,16 @@
-import { createElement } from 'react';
-import { FormattedHTMLMessage as IntlFormattedHTMLMessage } from 'react-intl';
-import { escapeId, isPhraseEnabled } from './functions';
+import {createElement} from 'react';
+import {
+    FormattedHTMLMessage as IntlFormattedHTMLMessage,
+} from 'react-intl';
 
+import {escapeId, isPhraseEnabled} from './functions'
 
 export class FormattedHTMLMessage extends IntlFormattedHTMLMessage {
-<<<<<<< HEAD
     render() {  
-=======
-    constructor(props: any, context: any) {
-        super(props, context);
-    }
-
-    render() {
->>>>>>> e523d2d6e86270716236185b54739805bd7d0520
         if (isPhraseEnabled()) {
             return createElement("span", null, escapeId(this.props.id));
         } else {
             return super.render();
-        }
+        }    
     }
 }
