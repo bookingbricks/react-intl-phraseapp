53a2d6667bd0da1819dd28e8f5a6bf4243810e3f
Jonatan Lundqvist Meden <2929793+jontelm@users.noreply.github.com>
2020-04-16 12:53:20 +0200

    init

--- src/functions.ts
+++ src/functions.ts
@@ -7,32 +7,36 @@ function sanitizeConfig(config: any) : any {
   return config;
 }
 
+const isClient = (typeof(window) !== 'undefined');
+
 export function initializePhraseAppEditor (config: any) {
   if (phraseAppEditor) return;
-  
   phraseAppEditor = true;
-  (<any>window).PHRASEAPP_ENABLED = config.phraseEnabled;  
-  (<any>window).PHRASEAPP_CONFIG = sanitizeConfig(config);
+  if (isClient) {
+    (<any>window).PHRASEAPP_ENABLED = config.phraseEnabled;
+    (<any>window).PHRASEAPP_CONFIG = sanitizeConfig(config);
 
-  if (config.phraseEnabled) {
-      const phraseapp = document.createElement('script');
-      phraseapp.type = 'text/javascript';
-      phraseapp.async = true;
-      phraseapp.src = ['https://', 'app.phrase.com/assets/in-context-editor/2.0/app.js?', new Date().getTime()].join('');
-      var s = document.getElementsByTagName('script')[0];
-      if (s !== undefined) {
-        s.parentNode.insertBefore(phraseapp, s);
-      } else {
-        document.body.appendChild(phraseapp);
+
+    if (config.phraseEnabled) {
+        const phraseapp = document.createElement('script');
+        phraseapp.type = 'text/javascript';
+        phraseapp.async = true;
+        phraseapp.src = ['https://', 'app.phrase.com/assets/in-context-editor/2.0/app.js?', new Date().getTime()].join('');
+        var s = document.getElementsByTagName('script')[0];
+        if (s !== undefined) {
+          s.parentNode.insertBefore(phraseapp, s);
+        } else {
+          document.body.appendChild(phraseapp);
+        }
       }
     }
 }
 
 export function isPhraseEnabled() : boolean {
-  return (<any>window).PHRASEAPP_ENABLED
+  return (isClient) ? (<any>window).PHRASEAPP_ENABLED : false;
 }
 
 export function escapeId (id : string | number) : string {
-  let config = (<any>window).PHRASEAPP_CONFIG;
+  let config = (isClient) ? (<any>window).PHRASEAPP_CONFIG : {};
   return  config.prefix + 'phrase_' + id + config.suffix;
 }
